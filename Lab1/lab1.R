c2281200ee9bdb16e806708f3239a4c2541a9979
nabil <nabiljesus@gmail.com>
2016-07-21 23:20:30 -0400

    graficos por codigo

--- Lab1/lab1.R
+++ Lab1/lab1.R
@@ -1,13 +1,19 @@
-#Andres Navarro
-#Nabil J. Márquez
-#Laboratorio 1
-#Estadistica Intensivo 2016
+##Andres Navarro   11-10688
+##Nabil J. Márquez 11-10683
+##Laboratorio 1
+##Estadistica Intensivo 2016
 
+#Leyendo los datos
 datos = read.table("IndiceCrim.dat", header=T)
-attach(datos)
+#attach(datos)
+#Como se encontró que es una mala practica, se decidio optar por:
+R<-datos$R
+W<-datos$W
+NW<-datos$NW
+#Liberando memoria
 keeps <- c("R", "W", "NW")
 datos <- datos[ , keeps, drop = FALSE]
-attach(datos)
+
 #Mean, Max, Min, 1st Qu, 3rd Qu., median
 mys <- summary(datos)
 #Calculando Desviación estandar
@@ -23,20 +29,33 @@ IQRS <- c(IQR(R),IQR(W),IQR(NW))
 IQRS <-paste("IQR.  :",IQRS)
 out<- rbind(out,IQRS)
 
-
+#Imprimiendo resultados
 rownames(out)<-rep("",each=dim(out)[1])
 print("==========Análisis descriptivo==========")
 print(out)
 
-par(mfrow=c(2, 3))
+#Generando los graficos
+
+#Grafico 1
+jpeg('TodosGraficos.jpg')
+par(mfrow=c(2, 3)) #6 graficos, 3 por fila
 histR <- hist(R, ylab="Frecuencia", xlab="Ind. Criminalidad", main="Delitos/ M. Hab.",xlim=c(0,200), ylim= c(0, 14))
 histW <- hist(W, ylab="Frecuencia", xlab="Ingreso familiar", main="Riqueza", ylim= c(0, 12))
 histNW <- hist(NW, ylab="Frecuencia", xlab="Nro de Per. No blancas", main="No-Blanc. / 1000 hab.", ylim= c(0, 25))
+boxplot(R, col= "Red", horizontal= F, xlab="Ind. Crim")
+boxplot(W, col= "Yellow", horizontal= F, xlab="Riqueza")
+boxplot(NW, col= "White", horizontal= F, xlab="Nro de Per. No blancas")
+dev.off()
+
+#Grafico 2
+jpeg('boxplotComparar.jpg', width=800, height=600)
+par(mfrow=c(1, 3)) #3 graficos, 3 por fila
 boxplot(R, col= "Red", horizontal= F, xlab="Ind. Crim",ylim= c(0, 700))
 boxplot(W, col= "Yellow", horizontal= F, xlab="Riqueza",ylim= c(0, 700))
 boxplot(NW, col= "White", horizontal= F, xlab="Nro de Per. No blancas",ylim= c(0, 700))
 dev.off()
 
+#Imprimiento histogramas
 print("==========Histograma R==========")
 print(histR)
 print("==========Histograma W==========")
@@ -44,3 +63,4 @@ print(histW)
 print("==========Histograma NW==========")
 print(histNW)
 
+#detach(datos)
